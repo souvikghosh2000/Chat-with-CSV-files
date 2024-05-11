8ea8f56ded84345f578f9ebf656c6decb8dffcbd
Souvik Ghosh <ghsouvik2000@gmail.com>
2024-05-11 17:38:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,89 @@
+
+
+
+import streamlit as st
+from streamlit_chat import message                      # For creating converational chat
+import tempfile
+from langchain.document_loaders.csv_loader import CSVLoader  # To load the CSV file
+from langchain_community.embeddings import HuggingFaceEmbeddings    # For embeddings
+from langchain_community.vectorstores import FAISS                  # To store the embeddings
+from langchain_community.llms import CTransformers                  # To load the model
+from langchain.chains import ConversationalRetrievalChain
+# import pandas as pd
+
+DB_FAISS_PATH = '/mnt/d/WSL/Chat-with-CSV-files/vectorstore/db_faiss_1'
+
+# Loading the model
+def load_llm():
+    # Load the locally downloaded model here
+    llm = CTransformers(
+        model = "/mnt/d/WSL/Chat-with-CSV-files/llama-2-7b-chat.ggmlv3.q4_K_S.bin",
+        model_type="llama",
+        max_new_tokens = 512,
+        temperature = 0.5
+    )
+    return llm
+
+st.title("Ask me Anything from your Data 🤖")
+
+
+uploaded_file = st.sidebar.file_uploader("Upload your Data", type="csv")
+
+if uploaded_file :
+   
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file_path = tmp_file.name
+
+    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={
+                'delimiter': ','})
+    data = loader.load()
+    #st.json(data)
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    db = FAISS.from_documents(data, embeddings)
+    db.save_local(DB_FAISS_PATH)
+    llm = load_llm()
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
+
+    def conversational_chat(query):
+        result = chain({"question": query, "chat_history": st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        return result["answer"]
+    
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey ! 👋"]
+        
+    # container for the chat history
+    response_container = st.container()
+    # container for the user's text input
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            
+            user_input = st.text_input("Query:", placeholder="Talk to your csv data here (:", key='input')
+            submit_button = st.form_submit_button(label='Send')
+            
+        if submit_button and user_input:
+            output = conversational_chat(user_input)
+            
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+
+
+
+    
